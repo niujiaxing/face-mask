f2e298fb3f6309982cb2d1a6d22d71e54bf9b597
xingstart <18342205009@163.com>
2020-04-26 10:17:30 +0800

    修改调用摄像头

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from werkzeug.utils import secure_filename
-from flask import Flask, render_template, jsonify, request, make_response, send_from_directory, abort
+from flask import Flask, render_template, jsonify, request, make_response, send_from_directory, abort, Response
 import os
 import base64
 from tensorflow_infer_niu import main
 from utils.distinguish_fish import get_fish
 from utils.distinguish_lajiclass import get_laji
-
+from utils.camera import VideoCamera
 
 
 app = Flask(__name__)
@@ -15,6 +15,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'mp4'])
 IMAGE = set(['png', 'jpg', 'JPG', 'PNG'])
 VIDEO = set(['mp4'])
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -65,10 +66,10 @@ def api_upload():
         return render_template("upload.html", msg="上传失败,请检查数据格式是否正确！")
 
 
-@app.route('/camera')
-def camera():
-    main(0, 0, 0)
-    return render_template("upload.html")
+# @app.route('/camera')
+# def camera():
+#     main(0, 0, 0)
+#     return render_template("upload.html")
 
 
 @app.route('/up_fish', methods=['POST'])
@@ -101,6 +102,27 @@ def sort_waste():
         return render_template("garbage.html", msg=msg)
     else:
         return render_template("garbage.html", msg="上传失败,请检查数据格式是否正确！")
+
+
+# 相机推流
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+# 相机喂流
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+# 当前实时相机画面
+@app.route('/camera')
+def cur_camera():
+    return render_template('camera.html')
 
 
 if __name__ == '__main__':
